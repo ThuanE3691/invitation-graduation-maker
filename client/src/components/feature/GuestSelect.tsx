782f4f6cad71112ab9b5397f037ed761d4507e65
Thuan <20520795@gm.uit.edu.vn>
2024-06-03 01:44:52 +0700

    Fixed something

--- client/src/components/feature/GuestSelect.tsx
+++ client/src/components/feature/GuestSelect.tsx
@@ -1,43 +1,43 @@
 import {
-    Select,
-    SelectContent,
-    SelectGroup,
-    SelectItem,
-    SelectLabel,
-    SelectTrigger,
-    SelectValue,
+	Select,
+	SelectContent,
+	SelectGroup,
+	SelectItem,
+	SelectLabel,
+	SelectTrigger,
+	SelectValue,
 } from "@/components/ui/select";
-import {ViewContext, ViewContextType} from "@/context/ViewContext";
-import {useContext} from "react";
-import {useNavigate} from "react-router-dom";
+import { ViewContext, ViewContextType } from "@/context/ViewContext";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 const GuestSelect = () => {
-    const {inviter} = useContext<ViewContextType>(ViewContext);
-    const navigate = useNavigate();
+	const { inviter } = useContext<ViewContextType>(ViewContext);
+	const navigate = useNavigate();
 
-    return (
-        <Select
-            onValueChange={(value) => {
-                navigate(`/user/${inviter.value?.name}/${value}`);
-            }}
-        >
-            <SelectTrigger className="w-full">
-                <SelectValue placeholder="Select guest want to modify" />
-            </SelectTrigger>
-            <SelectContent>
-                <SelectGroup>
-                    <SelectLabel>List guest</SelectLabel>
-                    {inviter?.value?.guests?.map((guest) => {
-                        return (
-                            <SelectItem key={guest.id} value={guest.name}>
-                                {guest.name}
-                            </SelectItem>
-                        );
-                    })}
-                </SelectGroup>
-            </SelectContent>
-        </Select>
-    );
+	return (
+		<Select
+			onValueChange={(value) => {
+				navigate(`/user/${inviter.value?.name}/${value}?displayMode=edit`);
+			}}
+		>
+			<SelectTrigger className="w-full">
+				<SelectValue placeholder="Select guest want to modify" />
+			</SelectTrigger>
+			<SelectContent>
+				<SelectGroup>
+					<SelectLabel>List guest</SelectLabel>
+					{inviter?.value?.guests?.map((guest) => {
+						return (
+							<SelectItem key={guest.id} value={guest.name}>
+								{guest.name}
+							</SelectItem>
+						);
+					})}
+				</SelectGroup>
+			</SelectContent>
+		</Select>
+	);
 };
 
 export default GuestSelect;
--- client/src/components/view/GuestView.tsx
+++ client/src/components/view/GuestView.tsx
@@ -2,9 +2,8 @@ import BaseView from "./BaseView";
 import ViewController from "../core/ViewController";
 import { useParams, useSearchParams } from "react-router-dom";
 import { ModeController } from "../core/ViewController";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ViewContext, ViewContextType } from "../../context/ViewContext";
-import { useQuery } from "@tanstack/react-query";
 import { BufferToBlob } from "@/lib/utils";
 import { Image } from "@/models";
 
@@ -23,41 +22,45 @@ const GuestView = () => {
 
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	const [searchParams, _] = useSearchParams();
+	const [isLoading, setIsLoading] = useState(true);
 
-	const { isPending } = useQuery({
-		queryKey: ["users", inviterName, guestName],
-		queryFn: async () => {
-			const users = await fetchUser();
-			const inviterData = await inviter.fetchInviter(users, inviterName);
-			const guestData = await guest.fetchGuest(inviterData ?? null, guestName);
-			inviter.setValue(inviterData);
-			guest.setValue(guestData);
-
-			const images: Image[] | undefined = guestData?.images?.map((image) => {
-				const blob = BufferToBlob(image.data.data, image.mimetype);
-				const imageUrl = URL.createObjectURL(blob);
-				return {
-					id: image.id,
-					filename: image.filename,
-					mimetype: image.mimetype,
-					url: imageUrl,
-					order: image.order,
-					data: image.data,
-					x: image.x,
-					y: image.y,
-					width: image.width,
-					height: image.height,
-					rotate: image.rotate,
-				} as Image;
+	useEffect(() => {
+		fetchUser()
+			.then((users) => {
+				return inviter.fetchInviter(users, inviterName);
+			})
+			.then((inviterData) => {
+				inviter.setValue(inviterData);
+				return guest.fetchGuest(inviterData ?? null, guestName);
+			})
+			.then((guestData) => {
+				guest.setValue(guestData);
+				const images: Image[] | undefined = guestData?.images?.map((image) => {
+					const blob = BufferToBlob(image.data.data, image.mimetype);
+					const imageUrl = URL.createObjectURL(blob);
+					return {
+						id: image.id,
+						filename: image.filename,
+						mimetype: image.mimetype,
+						url: imageUrl,
+						order: image.order,
+						data: image.data,
+						x: image.x,
+						y: image.y,
+						width: image.width,
+						height: image.height,
+						rotate: image.rotate,
+					} as Image;
+				});
+				setImages(images ?? []);
+				setGuestName(guestData?.name || "");
+			})
+			.finally(() => {
+				setIsLoading(false);
 			});
-			setImages(images ?? []);
-			setGuestName(guestData?.name || "");
-			return guestData;
-		},
-		retry: 3,
-	});
+	}, []);
 
-	if (isPending) {
+	if (isLoading) {
 		return <div></div>;
 	}
 
