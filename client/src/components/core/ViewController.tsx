b5998d856dfc695bb9671ce1d3ba93d180f80d09
Thuan <20520795@gm.uit.edu.vn>
2024-06-03 22:30:21 +0700

    Fix in client

--- client/src/components/core/ViewController.tsx
+++ client/src/components/core/ViewController.tsx
@@ -15,9 +15,12 @@ import GuestSelect from "../feature/GuestSelect";
 import { Button } from "../ui/button";
 import NewDialog from "../feature/NewDialog";
 import { useMutation } from "@tanstack/react-query";
-import {QRCodeCanvas} from "qrcode.react";
+import { QRCodeCanvas } from "qrcode.react";
 import axios from "@/lib/axiosInstance";
-import {getBlobFromImageElement, copyBlobToClipboard} from "copy-image-clipboard";
+import {
+	getBlobFromImageElement,
+	copyBlobToClipboard,
+} from "copy-image-clipboard";
 
 export enum ModeController {
 	ADMIN = "ADMIN",
@@ -46,91 +49,93 @@ const ViewController = ({ mode }: ViewControllerProps) => {
 		const qrElement = document.getElementById("qr-code");
 
 		if (!qrElement) {
-				return;
+			return;
 		}
 
 		getBlobFromImageElement(qrElement as HTMLImageElement)
-		.then((blob) => {
+			.then((blob) => {
 				return copyBlobToClipboard(blob);
-		})
-		.then(() => {
+			})
+			.then(() => {
 				console.log("Blob Copied");
-		})
-		.catch((e) => {
+			})
+			.catch((e) => {
 				console.log("Error: ", e.message);
-		});
-	}
-
-	
+			});
+	};
 
 	return (
-        <Sheet>
-            <SheetTrigger className="fixed z-50 py-4 pl-4">
-                <IoSettingsOutline size={32} />
-            </SheetTrigger>
-            <SheetContent>
-                <SheetHeader>
-                    <SheetTitle>Create new invitation page</SheetTitle>
-                </SheetHeader>
-                <Separator className="my-4" />
-                <div className="flex flex-col gap-y-4">
-                    <SheetHeader>
-                        <span className="font-medium">Current Inviter</span>
-                        <InviterSelect></InviterSelect>
-                    </SheetHeader>
-                    {mode === ModeController.INVITER && (
-                        <>
-                            <SheetHeader>
-                                <span className="font-medium">List Guest</span>
-                                <GuestSelect></GuestSelect>
-                            </SheetHeader>
-                            <div className="flex items-center gap-x-2">
-                                <Separator className=" w-[45%]" />
-                                <div className="font-medium font-montserrat">OR</div>
-                                <Separator className=" w-[45%]" />
-                            </div>
-                            <NewDialog inviter={inviter.value}></NewDialog>
-                        </>
-                    )}
-                    {mode === ModeController.GUEST && (
-                        <>
-                            <SheetHeader>
-                                <span className="font-medium">Guest Name</span>
-                                <div className="flex items-center gap-x-2">
-                                    <Input value={nameGuest?.value} onChange={nameGuest?.onChange}></Input>
-                                    <Button variant="outline" onClick={() => mutation.mutate()}>
-                                        Save
-                                    </Button>
-                                </div>
-                            </SheetHeader>
-                            <SheetHeader>
-                                <span className="font-medium">First Image</span>
-                                <ImageUpload image={images[0]}></ImageUpload>
-                            </SheetHeader>
-                            <SheetHeader>
-                                <span className="font-medium">Second Image</span>
-                                <ImageUpload image={images[1]}></ImageUpload>
-                            </SheetHeader>
-                            <SheetHeader>
-                                <span className="font-medium">Third Image</span>
-                                <ImageUpload image={images[2]}></ImageUpload>
-                            </SheetHeader>
-                            <SheetHeader>
-                                <span className="font-medium">QR Code Generate</span>
-                                <QRCodeCanvas
-																		id="qr-code"
-                                    value={`https://graduation-uit.vercel.app/user/${inviter.value?.name}/${guest.value?.name}`}
-                                ></QRCodeCanvas>
+		<Sheet>
+			<SheetTrigger className="fixed z-50 py-4 pl-4">
+				<IoSettingsOutline size={32} />
+			</SheetTrigger>
+			<SheetContent>
+				<SheetHeader>
+					<SheetTitle>Create new invitation page</SheetTitle>
+				</SheetHeader>
+				<Separator className="my-4" />
+				<div className="flex flex-col gap-y-4">
+					<SheetHeader>
+						<span className="font-medium">Current Inviter</span>
+						<InviterSelect></InviterSelect>
+					</SheetHeader>
+					{mode === ModeController.INVITER && (
+						<>
+							<SheetHeader>
+								<span className="font-medium">List Guest</span>
+								<GuestSelect></GuestSelect>
+							</SheetHeader>
+							<div className="flex items-center gap-x-2">
+								<Separator className=" w-[45%]" />
+								<div className="font-medium font-montserrat">OR</div>
+								<Separator className=" w-[45%]" />
+							</div>
+							<NewDialog inviter={inviter.value}></NewDialog>
+						</>
+					)}
+					{mode === ModeController.GUEST && (
+						<>
+							<SheetHeader>
+								<span className="font-medium">Guest Name</span>
+								<div className="flex items-center gap-x-2">
+									<Input
+										value={nameGuest?.value}
+										onChange={nameGuest?.onChange}
+									></Input>
+									<Button variant="outline" onClick={() => mutation.mutate()}>
+										Save
+									</Button>
+								</div>
+							</SheetHeader>
+							<SheetHeader>
+								<span className="font-medium">First Image</span>
+								<ImageUpload image={images[0]}></ImageUpload>
+							</SheetHeader>
+							<SheetHeader>
+								<span className="font-medium">Second Image</span>
+								<ImageUpload image={images[1]}></ImageUpload>
+							</SheetHeader>
+							<SheetHeader>
+								<span className="font-medium">Third Image</span>
+								<ImageUpload image={images[2]}></ImageUpload>
+							</SheetHeader>
+							<SheetHeader>
+								<span className="font-medium">QR Code Generate</span>
+								<QRCodeCanvas
+									id="qr-code"
+									value={`https://graduation-uit.vercel.app/user/${inviter.value?.name}/${guest.value?.name}`}
+								></QRCodeCanvas>
 
-																<Button variant="outline" onClick={onClickCopyQrCode}>
-																		Copy QR Code</Button>
-                            </SheetHeader>
-                        </>
-                    )}
-                </div>
-            </SheetContent>
-        </Sheet>
-    );
+								<Button variant="outline" onClick={onClickCopyQrCode}>
+									Copy QR Code
+								</Button>
+							</SheetHeader>
+						</>
+					)}
+				</div>
+			</SheetContent>
+		</Sheet>
+	);
 };
 
 export default ViewController;
